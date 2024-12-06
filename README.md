88ed346f84b8ea0de4b62228da38acec29ceeb39
Martin Eckardt <martin@holztechnik-markmueller.de>
2024-12-06 13:09:18 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# shell-translate
+# Translate Strings on Console
+
+This tool can be used to translate strings on console. Actually under the hood DeepL is used for the translation of strings. 
---
+++ shell-translate/Program.cs
@@ -0,0 +1,15 @@
+﻿using DeepL;
+using DotNetEnv;
+
+Env.Load();
+
+var authKey = Environment.GetEnvironmentVariable("AUTH_KEY");
+var translator = new Translator(authKey);
+
+var translatedText = await translator.TranslateTextAsync(
+    "Action required",
+    LanguageCode.English,
+    LanguageCode.German
+);
+
+Console.WriteLine(translatedText);
