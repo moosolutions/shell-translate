1a959af88f9c5a30985b02933983a7ef15b53a90
Martin Eckardt <martin@holztechnik-markmueller.de>
2024-12-09 12:54:04 +0100

    Add first command for translating strings

--- shell-translate/Program.cs
+++ shell-translate/Program.cs
@@ -1,15 +1,34 @@
-﻿using DeepL;
+﻿using System.Runtime.InteropServices.Swift;
+using DeepL;
+using DeepL.Model;
 using DotNetEnv;
+using Microsoft.Extensions.DependencyInjection;
+using shell_translate1.commands;
+using shell_translate1.di;
+using Spectre.Console.Cli;
 
-Env.Load();
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Env.Load();
 
-var authKey = Environment.GetEnvironmentVariable("AUTH_KEY");
-var translator = new Translator(authKey);
+        var authKey = Environment.GetEnvironmentVariable("AUTH_KEY");
 
-var translatedText = await translator.TranslateTextAsync(
-    "Action required",
-    LanguageCode.English,
-    LanguageCode.German
-);
+        var registrations = new ServiceCollection();
+        registrations.AddSingleton<Translator>(x => new Translator(authKey));
+        var registrar = new TypeRegistrar(registrations);
 
-Console.WriteLine(translatedText);
+        var app = new CommandApp(registrar);
+        app.Configure(config =>
+        {
+            config.AddCommand<TranslateTextCommand>("text")
+                .WithAlias("tt")
+                .WithDescription(@"Translates text from one language to another language.")
+                .WithExample("text", "Hello World!", "--from", "EN", "--to", "DE");
+        });
+
+        app.Run(args);
+    }
+}
+
---
+++ shell-translate/commands/TranslateTextCommand.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using DeepL;
+using DeepL.Model;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace shell_translate1.commands;
+
+public class TranslateTextCommand : Command<TranslateTextCommand.Settings>
+{
+    private readonly Translator _translator;
+    public class Settings : CommandSettings
+    {
+        [CommandOption("-f|--from <from>")]
+        [Description("Language from which to translate text.")]
+        public string From { get; set; }
+        
+        [CommandOption("-t|--to <to>")]
+        [DefaultValue(LanguageCode.German)]
+        [Description("Language to be translated.")]
+        public string To { get; set; }
+        
+        [CommandArgument(0, "<TEXT>")]
+        [Description("Text to be translated.")]
+        public string Text { get; set; }
+    }
+
+    public TranslateTextCommand(Translator translator)
+    {
+        _translator = translator ?? throw new ArgumentNullException(nameof(translator));
+    }
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        AnsiConsole.Status()
+            .Spinner(Spinner.Known.Star)
+            .Start("Translating...", ctx => {
+                var translated = TranslateText(settings.Text, settings.From, settings.To);
+                AnsiConsole.MarkupLine($"{settings.Text} = [blueviolet bold]{Markup.Escape(translated.Result.Text)}[/]");
+            });
+        return 0;
+    }
+
+    private async Task<TextResult> TranslateText(string text, string from, string to)
+    {
+        var translatedText = await _translator.TranslateTextAsync(
+            text, from, to
+        );
+        return translatedText;
+    }
+}
