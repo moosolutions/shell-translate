bcb9e4b6a2ce54ce00d6a890c6447a498b18f793
Martin Eckardt <martin@holztechnik-markmueller.de>
2024-12-10 16:17:04 +0100

    Add command for file translate

--- shell-translate/Program.cs
+++ shell-translate/Program.cs
@@ -14,9 +14,13 @@ public static class Program
         Env.Load();
 
         var authKey = Environment.GetEnvironmentVariable("AUTH_KEY");
+        var options = new TranslatorOptions
+        {
+            appInfo = new AppInfo { AppName = "shell-translate", AppVersion = "1.0.0" }
+        };
 
         var registrations = new ServiceCollection();
-        registrations.AddSingleton<Translator>(x => new Translator(authKey));
+        registrations.AddSingleton<Translator>(x => new Translator(authKey, options));
         var registrar = new TypeRegistrar(registrations);
 
         var app = new CommandApp(registrar);
@@ -26,6 +30,11 @@ public static class Program
                 .WithAlias("tt")
                 .WithDescription(@"Translates text from one language to another language.")
                 .WithExample("text", "Hello World!", "--from", "EN", "--to", "DE");
+            config.AddCommand<TranslateFileCommand>("file")
+                .WithAlias("f")
+                .WithDescription(@"Translates file from one language to another language.")
+                .WithExample("file", "C:\\translate.en.txt", "--output", "C:\\translate.de.txt", 
+                    "--from", "EN", "--to", "DE");
         });
 
         app.Run(args);
---
+++ shell-translate/commands/TranslateFileCommand.cs
@@ -0,0 +1,91 @@
+﻿using System.ComponentModel;
+using DeepL;
+using DeepL.Model;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace shell_translate1.commands;
+
+public class TranslateFileCommand : Command<TranslateFileCommand.Settings>
+{
+    private readonly Translator _translator;
+    
+    public class Settings : CommandSettings
+    {
+        [CommandOption("-f|--from <from>")]
+        [Description("Language from which to translate text.")]
+        public string From { get; set; }
+        
+        [CommandOption("-t|--to <to>")]
+        [DefaultValue(LanguageCode.German)]
+        [Description("Language to be translated.")]
+        public string To { get; set; }
+        
+        [CommandOption("-o|--output <output>")]
+        [Description("Path to output file.")]
+        public string OutputFile { get; set; }
+        
+        [CommandArgument(0, "<SourceFileName>")]
+        [Description("File to be translated.")]
+        public string SourceFileName { get; set; }
+    }
+
+    public TranslateFileCommand(Translator translator)
+    {
+        _translator = translator ?? throw new ArgumentNullException(nameof(translator));
+    }
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        AnsiConsole.Status()
+            .Spinner(Spinner.Known.Star)
+            .Start("Translating...", ctx => {
+                if (string.IsNullOrEmpty(settings.OutputFile))
+                {
+                    settings.OutputFile = Path.GetTempFileName();
+                }
+                var translated = TranslateFile(settings.SourceFileName, settings.OutputFile, settings.From, settings.To);
+                translated.Wait();
+                AnsiConsole.MarkupLine($"Source file: {settings.SourceFileName} translated to file [blueviolet bold]{settings.OutputFile}[/]");
+            });
+        return 0;
+    }
+
+    private async Task TranslateFile(string sourceFileName, string outputFile, string from, string to)
+    {
+        try
+        {
+            await _translator.TranslateDocumentAsync(
+                new FileInfo(sourceFileName),
+                new FileInfo(outputFile),
+                from,
+                to,
+                new DocumentTranslateOptions { Formality = Formality.More }
+            );
+        }
+        catch (DocumentTranslationException e)
+        {
+            if (e.DocumentHandle != null) {
+                var handle = e.DocumentHandle.Value;
+                AnsiConsole.MarkupLine("[yellow]Document uploaded correctly but isn't translated yet.[/]");
+                AnsiConsole.MarkupLine($"Document ID: {Markup.Escape(handle.DocumentId)}, Document key: {Markup.Escape(handle.DocumentKey)}");
+            }
+            else
+            {
+                AnsiConsole.Write(
+                    new FigletText("Exception while translating file.")
+                        .LeftJustified()
+                        .Color(Color.Red));
+
+                var panel = new Panel(e.Message)
+                {
+                    Border = BoxBorder.Rounded,
+                    Padding = new Padding(5, 5, 5, 5),
+                    Expand = true,
+                    Header = new PanelHeader("Exception message")
+                };
+                AnsiConsole.Render(panel);
+            }
+        }
+    }
+}
